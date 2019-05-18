498277a0cc431187ed450d3f70de96ec333c0bf5
Alasdair Hitchen <alasdairhitchen@gmail.com>
2019-05-18 11:48:03 +0100

    Changed variable names

--- yage/resources/shaders/basic.glsl
+++ yage/resources/shaders/basic.glsl
@@ -1,20 +1,17 @@
 [[VERTEX]]
 #version 430 core
-
-layout (location = 0) in vec4 in_pos;
+layout (location = 0) in vec3 buffer_pos;
 
 void main()
 {
-	gl_Position = vec4(in_pos.xyz, 1.0);
+    gl_Position = vec4(buffer_pos.xyz, 1.0);
 }
 
 [[FRAGMENT]]
 #version 430 core
-out vec4 fragColor;
-  
-uniform vec4 u_outColor;
+out vec4 fragColour;
 
 void main()
 {
-    fragColor = u_outColor;
+    fragColour = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }
